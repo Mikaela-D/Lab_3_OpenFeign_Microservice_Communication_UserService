6aa3f2eb25e74a2966a8d6fd8c0aa32b68f1605c
Mikaela-D <G00401392@atu.ie>
2023-10-11 15:21:57 +0100

    Added @Autowired code to UserController

--- src/main/java/atu/ie/lab3_userservice/UserController.java
+++ src/main/java/atu/ie/lab3_userservice/UserController.java
@@ -1,9 +1,15 @@
 package atu.ie.lab3_userservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jms.JmsProperties;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
     private RegistrationServiceClient registrationServiceClient;
-    private final AcknowledgeService acknowledgeService;
+
+    @Autowired
+    public UserController(RegistrationServiceClient registrationServiceClient) {
+        this.registrationServiceClient = registrationServiceClient;
+    }
 }
