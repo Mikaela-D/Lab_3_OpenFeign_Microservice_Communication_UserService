fb478096a8bed1fc3d306882885ce8c5ce17d2e1
Mikaela-D <G00401392@atu.ie>
2023-10-11 14:49:30 +0100

    Created new project UserService

---
+++ src/main/java/atu/ie/lab3_userservice/Lab3UserServiceApplication.java
@@ -0,0 +1,13 @@
+package atu.ie.lab3_userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab3UserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/atu/ie/lab3_userservice/Lab3UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.ie.lab3_userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
