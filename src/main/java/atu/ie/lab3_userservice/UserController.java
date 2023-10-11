0a508b53121bc4480b996240d3f97fc12d6c607e
Mikaela-D <G00401392@atu.ie>
2023-10-11 15:16:37 +0100

    Added @Rescontroller code to UserController

--- src/main/java/atu/ie/lab3_userservice/UserController.java
+++ src/main/java/atu/ie/lab3_userservice/UserController.java
@@ -1,4 +1,9 @@
 package atu.ie.lab3_userservice;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+    private RegistrationServiceClient registrationServiceClient;
+    private final AcknowledgeService acknowledgeService;
 }
